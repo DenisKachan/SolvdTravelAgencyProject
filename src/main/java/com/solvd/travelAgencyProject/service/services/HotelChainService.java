bd7c6f986da14d62e8ae262a9b78705a200e24b7
DenisKachan <den.kachan.98@mail.ru>
2024-01-03 16:10:54 +0300

    changes deleted

--- src/main/java/com/solvd/travelAgencyProject/service/services/HotelChainService.java
+++ src/main/java/com/solvd/travelAgencyProject/service/services/HotelChainService.java
@@ -1,16 +1,10 @@
 package com.solvd.travelAgencyProject.service.services;
 
 import com.solvd.travelAgencyProject.domain.HotelChain;
-import com.solvd.travelAgencyProject.persistence.utils.DOMParser;
 import com.solvd.travelAgencyProject.service.consoleScanner.CreationObjectsFromConsole;
-import com.solvd.travelAgencyProject.service.menu.MainMenu;
 import lombok.extern.log4j.Log4j2;
-import org.w3c.dom.Document;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
-import java.io.File;
 import java.io.IOException;
 import java.sql.SQLException;
 
@@ -18,20 +12,9 @@ import java.sql.SQLException;
 public class HotelChainService extends BaseService {
 
     public void createHotelChain() throws SQLException, IOException, SAXException {
-        if (MainMenu.domParserFlag) {
-            File hotelChainFile = new File(propertyReader.getProperty("hotelChainFile"));
-            DOMParser domParser = new DOMParser();
-            Document document = domParser.parse(hotelChainFile);
-            HotelChain hotelChain = new HotelChain();
-            NodeList names = document.getElementsByTagName("name");
-            Node name = names.item(0);
-            hotelChain.setName(name.getTextContent());
-            hotelChainJDBC.create(hotelChain).commit();
-        } else {
             CreationObjectsFromConsole creationObjectsFromConsole = new CreationObjectsFromConsole();
             HotelChain hotelChain = creationObjectsFromConsole.createNewHotelChainFromConsole();
             hotelChainJDBC.create(hotelChain).commit();
-        }
     }
 
     public void deleteHotelChainById() throws SQLException {
--- src/main/java/com/solvd/travelAgencyProject/service/services/MainTransportService.java
+++ src/main/java/com/solvd/travelAgencyProject/service/services/MainTransportService.java
@@ -1,16 +1,10 @@
 package com.solvd.travelAgencyProject.service.services;
 
 import com.solvd.travelAgencyProject.domain.MainTransport;
-import com.solvd.travelAgencyProject.persistence.utils.DOMParser;
 import com.solvd.travelAgencyProject.service.consoleScanner.CreationObjectsFromConsole;
-import com.solvd.travelAgencyProject.service.menu.MainMenu;
 import lombok.extern.log4j.Log4j2;
-import org.w3c.dom.Document;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
-import java.io.File;
 import java.io.IOException;
 import java.sql.SQLException;
 
@@ -18,20 +12,10 @@ import java.sql.SQLException;
 public class MainTransportService extends BaseService {
 
     public void createMainTransport() throws SQLException, IOException, SAXException {
-        if (MainMenu.domParserFlag) {
-            File mainTransportFile = new File(propertyReader.getProperty("mainTransportFile"));
-            DOMParser domParser = new DOMParser();
-            Document document = domParser.parse(mainTransportFile);
-            MainTransport mainTransport = new MainTransport();
-            NodeList names = document.getElementsByTagName("name");
-            Node name = names.item(0);
-            mainTransport.setName(name.getTextContent());
-            mainTransportJDBC.create(mainTransport).commit();
-        } else {
+
             CreationObjectsFromConsole creationObjectsFromConsole = new CreationObjectsFromConsole();
             MainTransport mainTransport = creationObjectsFromConsole.createNewMainTransportFromConsole();
             mainTransportJDBC.create(mainTransport).commit();
-        }
     }
 
     public void deleteMainTransportById() throws SQLException {
